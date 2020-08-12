a70aa947892ee64463b0db2d15c1012af074e842
Prakanshul Saxena <sheenusaxena2211@gmail.com>
2020-08-12 16:42:49 +0530

    about commit 1

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import About from './About';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <About />
   </React.StrictMode>,
   document.getElementById('root')
 );
