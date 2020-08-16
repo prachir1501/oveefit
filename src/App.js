2171ed73ca6dae8b3ea8cfbc7d6bba746b9cd229
Prachir Agrawal <prachiragrawal@MacBook-Pro.local>
2020-08-16 14:10:53 +0530

    team min commit done

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import {Switch,Route,Link} from "react-router-dom";
 import About from './About';
 import Bm from './bm';
+import Team from './team';
 
 function App() {
   return (
     <div>
      <Switch>
        <Route path="/about"><About/></Route>
-       <Route path="/team"><About/></Route>
+       <Route path="/team"><Team/></Route>
        <Route path="/business"><Bm/></Route>
        <Route path="/features"><About/></Route>
        <Route path="/hiring"><About/></Route>
