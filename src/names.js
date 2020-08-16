d70b8afc8e03ed8913dba62be9514d4befbbebde
Prachir Agrawal <prachiragrawal@MacBook-Pro.local>
2020-08-16 17:46:58 +0530

    modularize the team page

---
+++ src/names.js
@@ -0,0 +1,25 @@
+const names=[
+
+
+    {
+      "name":"Prachir",
+      "title":"Developer"
+    },
+
+    {
+        "name":"Manassi",
+        "title":"Developer"
+      },
+
+      {
+        "name":"Kanxu",
+        "title":"Developer"
+      },
+
+
+
+ ]
+
+ export default names;
+
+    
